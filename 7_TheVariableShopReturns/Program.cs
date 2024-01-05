0b6b9e5abe1623949ddc7545910b28f8c866e165
JohnnyR030T <johnnyvcrow@gmail.com>
2024-01-04 22:37:51 -0500

    Page 45: The Variable Shop & The Variable Shop Returns

---
+++ 7_TheVariableShopReturns/Program.cs
@@ -0,0 +1,46 @@
+﻿int intValue = 10;
+short shortValue = 20;
+long longValue = 1000000000000000000;
+byte byteValue = 255;
+sbyte sbyteValue = -127;
+uint uintValue = 4000000000;
+ushort ushortValue = 60000;
+ulong ulongValue = 1000000000000000000;
+float floatValue = 1.5f;
+double doubleValue = 1.5;
+decimal decimalValue = 1.5m;
+bool booleanValue = true;
+char charValue = 'A';
+string stringValue = "This is a string";
+
+intValue = 345;
+shortValue = 25;
+longValue = 2000000000000000000;
+byteValue = 128;
+sbyteValue = -17;
+uintValue = 3000000000;
+ushortValue = 50000;
+ulongValue = 9000000000000000000;
+floatValue = 34.8f;
+doubleValue = 2.9;
+decimalValue = 8.5m;
+booleanValue = false;
+charValue = '&';
+stringValue = "This is a new value string";
+
+Console.WriteLine(intValue);
+Console.WriteLine(shortValue);
+Console.WriteLine(longValue);
+Console.WriteLine(byteValue);
+Console.WriteLine(sbyteValue);
+Console.WriteLine(uintValue);
+Console.WriteLine(ushortValue);
+Console.WriteLine(ulongValue);
+Console.WriteLine(floatValue);
+Console.WriteLine(doubleValue);
+Console.WriteLine(decimalValue);
+Console.WriteLine(booleanValue);
+Console.WriteLine(charValue);
+Console.WriteLine(stringValue);
+
+
---
+++ TheVariableShop/Program.cs
@@ -0,0 +1,31 @@
+﻿int intValue = 10;
+short shortValue = 20;
+long longValue = 1000000000000000000;
+byte byteValue = 255;
+sbyte sbyteValue = -127;
+uint uintValue = 4000000000;
+ushort ushortValue = 60000;
+ulong ulongValue = 1000000000000000000;
+float floatValue = 1.5f;
+double doubleValue = 1.5;
+decimal decimalValue = 1.5m;
+bool booleanValue = true;
+char charValue = 'A';
+string stringValue = "This is a string";
+
+Console.WriteLine(intValue);
+Console.WriteLine(shortValue);
+Console.WriteLine(longValue);
+Console.WriteLine(byteValue);
+Console.WriteLine(sbyteValue);
+Console.WriteLine(uintValue);
+Console.WriteLine(ushortValue);
+Console.WriteLine(ulongValue);
+Console.WriteLine(floatValue);
+Console.WriteLine(doubleValue);
+Console.WriteLine(decimalValue);
+Console.WriteLine(booleanValue);
+Console.WriteLine(charValue);
+Console.WriteLine(stringValue);
+
+
