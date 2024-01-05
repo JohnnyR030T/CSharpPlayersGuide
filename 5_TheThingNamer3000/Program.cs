67724edd2d0790b0181ec8653d47c58fd19c2b46
JohnnyR030T <johnnyvcrow@gmail.com>
2024-01-04 19:17:52 -0500

    Page 31: The Thing Namer 3000

---
+++ 5_TheThingNamer3000/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine
+("What kind of thing are we talking about?");
+
+string a = Console.ReadLine(); // a is the name of the thing
+
+Console.WriteLine
+("How would you describe it? Big? Azure? Tattered?");
+
+string b = Console.ReadLine(); // b is the adjective
+string c = "Doom"; // c is the preposition
+string d = "3000"; // d is the noun
+
+
+
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
