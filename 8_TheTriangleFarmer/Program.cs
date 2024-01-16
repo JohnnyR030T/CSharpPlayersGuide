8b399478f60d935ca46f006a86009c375742458f
Johnathan Myers <johnnyvcrow@gmail.com>
2024-01-16 01:01:36 -0500

    Added TheTriangleFarmer challenge

---
+++ 8_TheTriangleFarmer/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter base of the triangle:");
+int baseInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter height of the triangle:");
+int heightInput = int.Parse(Console.ReadLine());
+
+double area = (baseInput * heightInput) / 2.0;
+
+Console.WriteLine($"The area of the triangle is: {area}");
