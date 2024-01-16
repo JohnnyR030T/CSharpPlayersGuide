bdcef01d9f940ed645453a8361f48187be6856eb
Johnathan Myers <johnnyvcrow@gmail.com>
2024-01-16 02:22:05 -0500

    Added TheFourSistersAndTheDuckbear project to solution

---
+++ TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int gatheredEggs = 0;
+int.TryParse(Console.ReadLine(), out gatheredEggs);
+
+if (gatheredEggs / 2 == 0)
+{
+    Console.WriteLine($"Each sister should get {gatheredEggs / 2} eggs.");
+}
+else
+{
+    Console.WriteLine($"Each sister should get {gatheredEggs / 2} eggs and the duckbear should get {gatheredEggs % 2} egg.");
+}
