44cff1c914789d2c39bb8cb83223c9a7325b154a
Johnathan Myers <johnnyvcrow@gmail.com>
2024-01-20 18:02:48 -0500

    Add project 11_TheDefenseOfConsolas to solution

---
+++ 11_TheDefenseOfConsolas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.WebSockets;
+
+Console.WriteLine("Target Row?: ");
+int targetRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Target Column?: ");
+int targetColumn = Convert.ToInt32(Console.ReadLine());
+
+var westSide = $"({targetRow}, {targetColumn - 1})";
+var eastSide = $"({targetRow}, {targetColumn + 1})";
+var northSide = $"({targetRow + 1}, {targetColumn})";
+var southSide = $"({targetRow - 1}, {targetColumn})";
+
+Console.WriteLine($"West Side: {westSide}");
+Console.WriteLine($"North Side: {northSide}");
+Console.WriteLine($"East Side: {eastSide}");
+Console.WriteLine($"South Side: {southSide}");
