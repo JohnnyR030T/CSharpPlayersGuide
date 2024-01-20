827beb11d736a0e6f307f4e9e42db1fd9842e789
Johnathan Myers <johnnyvcrow@gmail.com>
2024-01-20 18:13:16 -0500

    Add title, set deployment location logic, display results, and play a sound

--- 11_TheDefenseOfConsolas/Program.cs
+++ 11_TheDefenseOfConsolas/Program.cs
@@ -1,16 +1,32 @@
-﻿using System.Net.WebSockets;
+﻿
+// Display a title
+Console.Title = "Defense of Consolas";
+
 
 Console.WriteLine("Target Row?: ");
 int targetRow = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Target Column?: ");
 int targetColumn = Convert.ToInt32(Console.ReadLine());
 
+// Set deployment location logic
 var westSide = $"({targetRow}, {targetColumn - 1})";
 var eastSide = $"({targetRow}, {targetColumn + 1})";
 var northSide = $"({targetRow + 1}, {targetColumn})";
 var southSide = $"({targetRow - 1}, {targetColumn})";
 
+
+// Display the results
+Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine($"West Side: {westSide}");
+
+Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"North Side: {northSide}");
+
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine($"East Side: {eastSide}");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine($"South Side: {southSide}");
+
+// Play a sound
+Console.Beep();
