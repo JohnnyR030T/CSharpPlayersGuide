107b7c27952c0e3253260d698f9918c6eecd2b77
Johnathan Myers <johnnyvcrow@gmail.com>
2024-01-20 19:41:32 -0500

    Added Project 13_Watchtower

---
+++ 12_RepairingTheClocktower/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Title = "Repairing The Clocktower";
+
+Console.WriteLine("Enter a number: ");
+int theNumber = Convert.ToInt32(Console.ReadLine());
+
+if (theNumber % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
---
+++ 13_Watchtower/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Enter a positive or negative number for x value: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a positive or negative number for y value: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+if (x == 0 && y == 0)
+{
+    Console.WriteLine("The enemy is here!");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("The enemy is in the North West!");
+}
+else if (x == 0 && y > 0)
+{
+    Console.WriteLine("The enemy is in the North!");
+}
+else if (x > 0 && y > 0)
+{
+    Console.WriteLine("The enemy is in the North East!");
+}
+else if (x > 0 && y == 0)
+{
+    Console.WriteLine("The enemy is in the East!");
+}
+else if (y == 0 && x < 0){
+    Console.WriteLine("The enemy is in the West!");
+}
+else if(y == 0 && x > 0){
+    Console.WriteLine("The enemy is in the East!");
+}
+else if (y < 0 && x < 0)
+{
+    Console.WriteLine("The enemy is in the South West!");
+}
+else if (y < 0 && x == 0)
+{
+    Console.WriteLine("The enemy is in the South!");
+}
+else if (y < 0 && x > 0)
+{
+    Console.WriteLine("The enemy is in the South East!");
+}
+else
+{
+    Console.WriteLine("The enemy is not here!");
+}
